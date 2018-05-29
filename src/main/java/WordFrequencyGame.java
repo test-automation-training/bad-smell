d69848bbbe2a6ff627311731559e981e83175a7e
hhu <hhu@thoughtworks.com>
2018-05-29 17:09:46 +0800

    refactor done

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,62 +1,36 @@
 import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
 
 /**
  * Created by jxzhong on 2018/5/22.
  */
-public class WordFrequencyGame {
-    public String getResult(String inputStr) {
+class WordFrequencyGame {
 
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
-        } else {
+    private static final String SPLIT_REGEX = "\\s+";
+    private static final String RECEIPT_DELIMITER = "\n";
 
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Word> words = new ArrayList<>();
-                for (String s : arr) {
-                    Word word = new Word(s, 1);
-                    words.add(word);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Word>> map = getListMap(words);
-
-                List<Word> list = new ArrayList<>();
-                for (Map.Entry<String, List<Word>> entry : map.entrySet()) {
-                    Word word = new Word(entry.getKey(), entry.getValue().size());
-                    list.add(word);
-                }
-                words = list;
-
-                words.sort((w1, w2) -> w2.getCount() - w1.getCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Word w : words) {
-                    String s = w.getText() + " " + w.getCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
-        }
+    String play(String input) {
+        return buildReceipt(buildWords(buildTexts(input)));
     }
 
-    private Map<String, List<Word>> getListMap(List<Word> words) {
-        Map<String, List<Word>> map = new HashMap<>();
-        for (Word word : words) {
-//       map.computeIfAbsent(word.getText(), k -> new ArrayList<>()).add(word);
-            if (!map.containsKey(word.getText())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(word);
-                map.put(word.getText(), arr);
-            } else {
-                map.get(word.getText()).add(word);
-            }
-        }
-        return map;
+    private List<String> buildTexts(String input) {
+        return Arrays.asList(input.split(SPLIT_REGEX));
+    }
+
+    private List<Word> buildWords(List<String> texts) {
+        return texts.stream()
+                .collect(groupingBy(text -> text))
+                .entrySet().stream()
+                .map(e -> new Word(e.getKey(), e.getValue().size()))
+                .collect(Collectors.toList());
+    }
+
+    private String buildReceipt(List<Word> sizedWords) {
+        return sizedWords.stream()
+                .sorted((w1, w2) -> w2.getCount() - w1.getCount())
+                .map(w -> w.getText() + " " + w.getCount())
+                .collect(joining(RECEIPT_DELIMITER));
     }
 }
