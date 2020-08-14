403f1f2d46eb37b302d5e86d6a51ab6d148d5f05
Hao Hu <huhao@outlook.com>
2020-08-14 22:49:11 +0800

    update anwser

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,35 +1,35 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.*;
 
-class WordFrequencyGame {
+public class WordFrequencyGame {
 
-    private static final String SPLIT_REGEX = "\\s+";
-    private static final String RECEIPT_DELIMITER = "\n";
+    private static final String WORD_SPLITTER = "\\s+";
+    private static final String RESULT_DELIMITER = "\n";
+    private static final String VIEW_TEMPLATE = "%s %d";
 
-    String play(String input) {
-        return buildReceipt(buildWords(buildTexts(input)));
+    public String play(String inputStr) {
+        return generateResult(accumulate(split(inputStr)));
     }
 
-    private List<String> buildTexts(String input) {
-        return Arrays.asList(input.split(SPLIT_REGEX));
+    private String generateResult(Map<String, Integer> accumulateWords) {
+        return accumulateWords.entrySet().stream()
+                .sorted((right, left) -> left.getValue() - right.getValue())
+                .map(wordFrequency -> String.format(VIEW_TEMPLATE, wordFrequency.getKey(), wordFrequency.getValue()))
+                .collect(joining(RESULT_DELIMITER));
     }
 
-    private List<Word> buildWords(List<String> texts) {
-        return texts.stream()
-                .collect(groupingBy(text -> text))
+    private Map<String, Integer> accumulate(List<String> splitWords) {
+        return splitWords.stream()
+                .collect(groupingBy(word -> word))
                 .entrySet().stream()
-                .map(e -> new Word(e.getKey(), e.getValue().size()))
-                .collect(Collectors.toList());
+                .collect(toMap(Map.Entry::getKey, entry -> entry.getValue().size()));
     }
 
-    private String buildReceipt(List<Word> sizedWords) {
-        return sizedWords.stream()
-                .sorted((w1, w2) -> w2.getCount() - w1.getCount())
-                .map(w -> w.getText() + " " + w.getCount())
-                .collect(joining(RECEIPT_DELIMITER));
+    private List<String> split(String inputStr) {
+        return Arrays.asList(inputStr.split(WORD_SPLITTER));
     }
+
 }
